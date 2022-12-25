61f83e91bc7c7f60053a6bfe0cbe8bc244065668
Petingoso <petingavasco@protonmail.com>
2022-12-25 15:09:23 +0000

    again reuploaded, srcery theme done

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -30,7 +30,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme nord]]
+vim.cmd [[colorscheme everforest]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
