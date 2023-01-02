5a207a75a98943cfd3498aa583c823575eec32c3
Petingoso <petingavasco@protonmail.com>
2023-01-02 21:09:19 +0000

    tweaks and added batterys script

--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -31,7 +31,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme nord]]
+vim.cmd [[colorscheme srcery]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
